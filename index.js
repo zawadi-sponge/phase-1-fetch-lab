9328b377aa1c19c94775579a48c57253fc976595
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-23 15:08:23 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let result = fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(books => renderBooks(books))
+  return result;
 }
 
 function renderBooks(books) {
